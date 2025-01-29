33a9f42f6df6ab7e94c7c05f05927626e86f912d
Afranio Nunes Dantas <afranio.dantas@livsaude.com.br>
2025-01-29 14:42:33 -0300

    feat: create component button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from "./components/button";
+
 export function App() {
   return (
     <>
-      <div className="App">Hello World</div>
+      <div className="App">
+        <Button />
+      </div>
     </>
   );
 }
